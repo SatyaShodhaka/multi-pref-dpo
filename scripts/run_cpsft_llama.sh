4101920d4feb6289970bdddd4fc8585ccd6ec258
Satya Shodhaka Rekha Prabhanjan <satyashodhaka66@gmail.com>
2025-03-24 12:45:30 -0400

    removed deepspeed config

--- scripts/run_cpsft_llama.sh
+++ scripts/run_cpsft_llama.sh
@@ -1,11 +1,14 @@
-deepspeed --num_gpus=1 ./src/CPSFT/cpsft/train_sft_llama.py\
+#!/bin/bash
+
+python ./src/CPSFT/cpsft/train_sft_llama.py \
   --data_path ./data/ultrafeedback_csft.jsonl \
-  --deepspeed ./src/CPSFT/cpsft/deepspeed_config/ZeRO_2.json \
   --output_dir ./src/data/checkpoints/llama_sft/ \
   --eval_steps 20 \
   --save_steps 100 \
   --base_model meta-llama/Llama-3.2-1B-Instruct \
   --prompt_template_name llama_1b_instruct \
-  # --lora_r 8 \
-  # --lora_alpha 16 \
-  # --lora_dropout 0.05
+  --per_device_train_batch_size 8 \
+  --gradient_accumulation_steps 32 \
+  --learning_rate 1e-5 \
+  --num_train_epochs 3 \
+  --val_set_size 500
