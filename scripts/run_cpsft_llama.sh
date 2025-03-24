7c874f26047d863593a65e9d6d6a10129d035a00
Satya Shodhaka Rekha Prabhanjan <satyashodhaka66@gmail.com>
2025-03-24 14:36:59 -0400

    updated batch size

--- scripts/run_cpsft_llama.sh
+++ scripts/run_cpsft_llama.sh
@@ -7,7 +7,7 @@ python ./src/CPSFT/cpsft/train_sft_llama.py \
   --save_steps 100 \
   --base_model meta-llama/Llama-3.2-1B-Instruct \
   --prompt_template_name llama_1b_instruct \
-  --per_device_train_batch_size 32 \
+  --per_device_train_batch_size 8 \
   --gradient_accumulation_steps 32 \
   --learning_rate 1e-5 \
   --num_train_epochs 3 \
